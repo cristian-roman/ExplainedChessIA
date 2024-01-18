23b0ba8369fd351a01007cdf932061387fb5e0a1
Cristian Roman <romancristianroman@yahoo.com>
2024-01-18 08:38:08 +0200

    tema updatata

--- Tema2/main.py
+++ Tema2/main.py
@@ -1,10 +1,10 @@
 import chess.pgn
-#
+
 # with open('games.txt', 'r') as file:
 #     games_text = file.read()
 #
 # games_text.replace('\r\n', ' ')
-
+#
 # partide = games_text.split('#')
 # nr_partida = 0
 # for partida in partide:
@@ -22,28 +22,53 @@ import chess.pgn
 #                 file.write(str(spatiu//2 + 1) + '. ' + mutare + ' ')
 #             if spatiu % 2 == 0:
 #                 file.write(mutare + ' ')
-#         file.write(mutari[-1] + '# ')
-#         if spatiu % 2 == 1:
+#
+#         if spatiu % 2 == 0:
+#             file.write(str(spatiu//2 + 1) + '. ' + mutari[-1] + '# ')
 #             file.write('*')
+#
+#         else:
+#             file.write(mutari[-1] + '# ')
 
 
 counter = dict()
+
 for i in range(1, 6326):
-    pgn = open(f"partida_{i}.pgn")
+    file_path = f"partida_{i}.pgn"
 
-    game = chess.pgn.read_game(pgn)
+    with open(file_path, "r") as pgn_file:
+        game = chess.pgn.read_game(pgn_file)
 
-    last_3_moves = []
-    for move in game.mainline_moves():
-        last_3_moves.append(move)
-        if len(last_3_moves) > 3:
-            last_3_moves.pop(0)
+        if game is not None:
+            last_6_moves = []
 
-        if len(last_3_moves) == 3:
-            key = tuple(last_3_moves)
-            counter[key] = counter.get(key) + 1
+            for move in game.mainline_moves():
+                last_6_moves.append(move)
+
+                if len(last_6_moves) > 6:
+                    last_6_moves.pop(0)
+
+            if len(last_6_moves) == 6:
+                sep = 0
+                first_player_moves = []
+                second_player_moves = []
+
+                for move in last_6_moves:
+                    sep += 1
+
+                    if sep % 2 == 1:
+                        first_player_moves.append(move)
+                    else:
+                        second_player_moves.append(move)
+
+                first_player_moves_as_string = " ".join(str(move) for move in first_player_moves)
+                second_player_moves_as_string = " ".join(str(move) for move in second_player_moves)
+
+                counter[first_player_moves_as_string] = counter.get(first_player_moves_as_string, 0) + 1
+                counter[second_player_moves_as_string] = counter.get(second_player_moves_as_string, 0) + 1
 
 for key in sorted(counter, key=counter.get, reverse=True):
     value = counter[key]
+
     if value > 2:
         print(key, value)
