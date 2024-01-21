514544349673204c7fd4f811f5742e235ad32621
Cristian Roman <romancristianroman@yahoo.com>
2024-01-21 23:27:14 +0200

    minimal loss automatic updatE

--- ExplainedChess/AI/Trainer.py
+++ ExplainedChess/AI/Trainer.py
@@ -6,10 +6,11 @@ from AI.Utils.Utils import Utils
 
 
 class Trainer:
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, minimal_loss_path="./AI/loss.txt"):
         self.data = Utils.load_dataset(dataset_path)
         self.dataset_length = len(self.data)
 
+        self.minimal_loss = 100000
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         print("Do you want to load the model from a file? (y/n)")
@@ -17,13 +18,15 @@ class Trainer:
         if answer == 'y':
             self.model = Model().to(self.device)
             self.model.load_state_dict(torch.load("./AI/model.pth"))
+
+            with open(minimal_loss_path, 'r') as f:
+                self.minimal_loss = float(f.readline())
         else:
             self.model = Model().to(self.device)  # Move the model to GPU
         self.criterion = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
 
     def train(self):
-        minimal_loss = 0.006283239782403527
         total_loss = 0.0
         for epoch in range(1, 10000):
             current_loss = 0.0
@@ -46,12 +49,12 @@ class Trainer:
 
             total_loss += current_loss
             if epoch % 10 == 0:
-                print(f'Epoch: {epoch}, average_loss: {total_loss/10}\n')
+                print(f'Epoch: {epoch}, average_loss: {total_loss / 10}\n')
                 total_loss = 0.0
 
-            if current_loss < minimal_loss:
-                minimal_loss = current_loss
+            if current_loss < self.minimal_loss:
+                self.minimal_loss = current_loss
                 torch.save(self.model.state_dict(), "./AI/model.pth")
-                print(f"best model saved with loss: {minimal_loss} at epoch: {epoch}")
+                print(f"best model saved with loss: {self.minimal_loss} at epoch: {epoch}")
                 with open("./AI/loss.txt", 'w') as f:
-                    f.write(str(minimal_loss))
+                    f.write(str(self.minimal_loss))
