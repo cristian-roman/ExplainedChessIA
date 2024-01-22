9645c20f410d38cf25868d26e7349f756427b546
Cristian Roman <romancristianroman@yahoo.com>
2024-01-22 11:06:21 +0200

    not good

--- ExplainedChess/AI/Trainer.py
+++ ExplainedChess/AI/Trainer.py
@@ -24,7 +24,7 @@ class Trainer:
         else:
             self.model = Model().to(self.device)  # Move the model to GPU
         self.criterion = nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0000001)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.000001)
 
     def train(self):
         total_loss = 0.0
